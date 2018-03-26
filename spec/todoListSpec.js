61a337fc618297af2ccd39a6e8a0bffd9616974d
Alexey Breslavsky <ABreslavsky@tula.co>
2018-03-26 04:36:36 -0700

    1-added jacmin and karma; 2-added local vue

---
+++ spec/todoListSpec.js
@@ -0,0 +1,13 @@
+﻿describe("todoList", function () {
+
+    it("test should return  string 'test'", function (done) {
+        require(["todoList"], function (todoList) {
+            
+            var result = todoList.test();
+
+            expect(result).toEqual('test');
+
+            done();
+        });
+    });
+});
